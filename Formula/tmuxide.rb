1e02da79f0b345f692cefaf0196de16563a4b373
Frank J. Mattia <frankjmattia@gmail.com>
2014-06-17 16:32:37 -0400

    tmuxide: Release v0.0.3.k

--- Formula/tmuxide.rb
+++ Formula/tmuxide.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Tmuxide < Formula
   homepage 'https://github.com/frankjmattia/tmuxide'
-  url 'https://frankjmattia.github.io/tmuxide/tmuxide-0.0.3.j.tar.gz'
-  sha1 '5e51b66013a4afd60c362ce448b7e976cf7d6d6e'
+  url 'https://frankjmattia.github.io/tmuxide/tmuxide-0.0.3.k.tar.gz'
+  sha1 'ea249f6aa97fda56380f3fb72495dea82b3cb2c2'
 
   depends_on 'gnu-getopt'
 
