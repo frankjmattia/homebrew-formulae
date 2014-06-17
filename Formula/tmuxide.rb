df1d738fb86cf5c29f181db476bdb3f263c1db5f
Frank J. Mattia <frankjmattia@gmail.com>
2014-06-16 22:10:17 -0400

    tmuxide: Release v0.0.3.j

--- Formula/tmuxide.rb
+++ Formula/tmuxide.rb
@@ -2,33 +2,19 @@ require "formula"
 
 class Tmuxide < Formula
   homepage 'https://github.com/frankjmattia/tmuxide'
-  url 'https://frankjmattia.github.io/tmuxide/tmuxide-0.0.3.i.tar.gz'
-  sha1 '8136215625d6f9fb6f3173a6d77f14e0b4c1ea88'
+  url 'https://frankjmattia.github.io/tmuxide/tmuxide-0.0.3.j.tar.gz'
+  sha1 '5e51b66013a4afd60c362ce448b7e976cf7d6d6e'
 
   depends_on 'gnu-getopt'
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system './configure', '--disable-debug',
-                          '--disable-dependency-tracking',
-                          '--disable-silent-rules',
+    system './configure', '--disable-silent-rules',
                           "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system 'make', 'install' # if this fails, try separate make/make install steps
+
+    system 'make', 'install'
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test tmuxide`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system 'false'
+    system 'tmuxide --version'
   end
 end
